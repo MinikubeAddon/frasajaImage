e2258df4c5d8bef3c1af273570a6aa2194f6ee47
sarahheacock <seheacock@bellsouth.net>
2018-04-04 20:10:52 -0400

    made webpack production

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: './client/index.js',
@@ -26,6 +27,10 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin("styles.css"),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ]
 };
